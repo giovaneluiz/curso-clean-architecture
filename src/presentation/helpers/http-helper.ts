ba54a933b5b2f8a990ef077bdda2dbc8a4b781a8
giovaneluiz <35082905+giovaneluiz@users.noreply.github.com>
2020-08-07 02:09:19 -0300

    feat: testando retorno 200 em caso de sucesso na classse signup controller

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -5,6 +5,7 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
