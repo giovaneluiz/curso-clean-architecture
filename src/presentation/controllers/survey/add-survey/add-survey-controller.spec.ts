9a0fbf87ed129f12e49d3edb75ab0527d832ecab
giovaneluiz <35082905+giovaneluiz@users.noreply.github.com>
2020-09-16 18:25:06 -0300

    refactor: move AddSurvey from sut types

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -11,20 +11,29 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
-// const makeValidationStub = (): Validation => {
+interface SutTypes {
+  sut: AddSurveyController
+  validationStub: Validation
+}
 
-// }
+const makeSut = (): SutTypes => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  const validationStub = new ValidationStub()
+  const sut = new AddSurveyController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
 
 describe('AddSurvey Controller', () => {
   test('Should call Validation with correct values', async () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return null
-      }
-    }
-    const validationStub = new ValidationStub()
+    const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const sut = new AddSurveyController(validationStub)
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
