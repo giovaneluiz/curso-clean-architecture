567a1179d30ccf8b34400b50e6df3c8e9ccb73fb
giovaneluiz <35082905+giovaneluiz@users.noreply.github.com>
2020-09-16 19:10:05 -0300

    test: ensure return 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,4 +1,4 @@
-import { serverError } from '../../../helpers/http/http-helper'
+import { noContent, serverError } from '../../../helpers/http/http-helper'
 import { AddSurveyController } from './add-survey-controller'
 import { AddSurvey, badRequest, HttpRequest, HttpResponse, Validation, AddSurveyModel } from './add-survey-controller-protocols'
 
@@ -79,5 +79,10 @@ describe('AddSurvey Controller', () => {
     jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
