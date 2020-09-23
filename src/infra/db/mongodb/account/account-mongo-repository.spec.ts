5c2878d2d14e47547dbb8e5b07b6f4b7de65bdf8
giovaneluiz <35082905+giovaneluiz@users.noreply.github.com>
2020-09-22 21:29:00 -0300

    test: ensure return null if loadByToken fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -74,7 +74,7 @@ describe('Account Mongo Repository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
-  describe('loadByEmail', () => {
+  describe('loadByToken', () => {
     test('Should return an account on loadByToken withount role success', async () => {
       const sut = makeSut()
       await accountCollection.insertOne({
@@ -105,6 +105,11 @@ describe('Account Mongo Repository', () => {
       expect(account.name).toBe('any_name')
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
+    })
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
     })
   })
 })
